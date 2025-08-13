19048e8bb467bf15045917b25031c39f8c5ddb51
Eddie-Hunt <178103@cloud.rangitoto.school.nz>
2025-08-14 09:22:04 +1200

    time tracker
    This program is a simple program that converts 24 hour time to 12 hour time

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+num = 0
+time = int(input("whats the time"))
+if time <= 12 and time > 0:
+  print(f"The time is {time}AM")
+elif time >= 24 and time > 12:
+  num = time - 12
+  print(f"The time is {num}PM")
